94fc13da2697bc03fabe0c15bdda6fae999bc0c7
Vibhor <elementswaer@gmail.com>
2022-01-06 21:23:20 -0800

    added more functionality to gif command

--- commands/gif.js
+++ commands/gif.js
@@ -3,15 +3,15 @@ const axios = require('axios');
 exports.run = async (bot, message, argument) => {
     let keyword = argument[0];
     
-    tenorurl = `https://g.tenor.com/v1/search?q=${keyword}&key=${process.env.TENORTOKEN}&limit=8`;
+    tenorurl = `https://g.tenor.com/v1/search?q=${keyword}&key=${process.env.TENORTOKEN}&limit=49`;
     let fetchTenorAPI = await axios.get(tenorurl);
     let tenordata = await fetchTenorAPI.data;
-    let index = Math.floor(Math.random() * 8);
+    let index = Math.floor(Math.random() * 49);
     if(argument.length == 0){
         console.log(tenordata);
         message.channel.send(tenordata.results[index].url);
     }
-    else if(!(keyword == "") && argument[1] == "dm"){
+    else if(!(keyword == "") && argument[argument.length - 1] == "dm"){
         message.author.send(tenordata.results[index].url);
     }
     else if(!(keyword == "")){
